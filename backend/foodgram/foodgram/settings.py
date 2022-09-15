706c6407cab26e19606f1918dd107ef7e2b47e42
Hrenly <hrenly@gmail.com>
2022-09-15 11:07:55 +0300

    build(backend): Debug mode – false

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "v%2wcdk%azzk6yd+^v)t&ubz+h9^#&b5(p4+nott3*_nj)q1mk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
